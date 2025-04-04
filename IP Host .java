e6ab41b319f04454d2493986a53d14618870ea29
DINU <nirujogidinesh@gmail.com>
2025-04-04 13:37:12 +0530

    Update IP Host .java

--- IP Host .java
+++ IP Host .java
@@ -1,14 +1,9 @@
 import java.net.*;
 
-public class Demo {
-    public static void main(String[] args) {
-        try {
-            InetAddress my_address = InetAddress.getLocalHost();
-            System.out.println("The IP address is: " + my_address.getHostAddress());
-            System.out.println("The host name is: " + my_address.getHostName());
-        } 
-        catch (UnknownHostException e) {
-            System.out.println("Couldn't find the local address.");
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        InetAddress localHost = InetAddress.getLocalHost();
+        System.out.println("Hostname: " + localHost.getHostName());
+        System.out.println("IP Address: " + localHost.getHostAddress());
     }
 }
