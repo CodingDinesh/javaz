5d1c7066a7bfdf58de04db4597e4f76e8c9e402c
DINU <nirujogidinesh@gmail.com>
2025-04-04 13:38:00 +0530

    Update IP Address.java

--- IP Address.java
+++ IP Address.java
@@ -1,20 +1,7 @@
 import java.net.*;
 
-class IpAddr {
-    public static void main(String args[]) {
-        String s = "https://www.yahoo.com/";
-
-        try {
-            // Extracting the hostname from the URL
-            String host = new URL(s).getHost();
-            InetAddress ip = InetAddress.getByName(host);
-            System.out.println("Public IP Address of " + host + " : " + ip.getHostAddress());
-        } 
-        catch (MalformedURLException e) {
-            System.out.println("Invalid URL");
-        } 
-        catch (UnknownHostException e) {
-            System.out.println("Unknown Host");
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(InetAddress.getByName("www.yahoo.com").getHostAddress());
     }
 }
