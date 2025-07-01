2ff59babca5735f99cab903944ae351a3275ce87
DINU <nirujogidinesh@gmail.com>
2025-07-01 18:46:18 +0530

    Create relation operators .java

---
+++ relation operators .java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main
+{
+public static void main(String  args[])
+{
+Scanner sc=new Scanner(System.in);
+int a,b,c,d,e,f,g,h;
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+d=a*b;
+e=a-b;
+f=a/b;
+g=a%b;
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+}}
