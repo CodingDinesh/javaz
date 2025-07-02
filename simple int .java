3054ec586cf1a941b9f172d7cb50b3cf094f970c
DINU <nirujogidinesh@gmail.com>
2025-07-02 14:01:29 +0530

    Create simple int .java

---
+++ simple int .java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c,d,e;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=a*b*c;
+        e=d/100;
+        System.out.println(e);
+    }
+}
