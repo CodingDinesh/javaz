75fedc515844822627ca8e206bce57a84664aacc
DINU <nirujogidinesh@gmail.com>
2025-08-06 11:28:00 +0530

    Create table .java

---
+++ table .java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+ public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     for(int i=1;i<=10;i++)
+     {
+        int m=n*i;
+ System.out.println(n+" x "+i+" = "+m);
+ 
+     }}
+}
